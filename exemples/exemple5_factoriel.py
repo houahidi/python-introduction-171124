d185aa30d1115731237c478cd02126953f3f9503
houahidi <houahidi@uni-consulting.fr>
2017-11-24 16:41:37 +0100

    exemples de la session 27/11/17

--- exemples/exemple5_factoriel.py
+++ exemples/exemple5_factoriel.py
@@ -1,11 +1,20 @@
-chiffre = input('Vas y!! A toi de jouer! Saisis une suite de chiffres : ')
-#conversion
 
-factoriel = int(chiffre)
-affichage = "" + str(chiffre)
-for elem in range(int(chiffre) -1, 1, -1):
-    affichage += "*" + str(elem)
-    factoriel = factoriel * elem
-    #print ("A ce stade le factoriel est ", factoriel,".")
-print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
-print("La factoriel de ", chiffre,":", affichage,"=",factoriel,".")
+def factoriel(chiffre):
+    factoriel = chiffre
+    affichage = "" + str(chiffre)
+    for elem in range(int(chiffre) -1, 1, -1):
+        affichage += "*" + str(elem)
+        factoriel = factoriel * elem
+        #print ("A ce stade le factoriel est ", factoriel,".")
+    print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+    print("La factoriel de ", chiffre,":", affichage,"=",factoriel,".")
+
+# appel de la fonction
+
+#factoriel d'un chiffre demande a l'utilisateur
+chiffre = input('Vas y!! A toi de jouer! Saisis une suite de chiffres : ')
+# conversion
+chiffre = int(chiffre)
+factoriel(chiffre)
+#factoriel d'un chiffre connu
+factoriel(1000)
--- exemples/exemple6_suite_fibonacci.py
+++ exemples/exemple6_suite_fibonacci.py
@@ -1,13 +1,22 @@
-# Afficher si chiffre pair ou impair
+# valeur par defaut d'un parametre
+def fibonacci(rang=3):
+    # Afficher si chiffre pair ou impair
+    U0 = 1
+    U1 = 1
+
+    for indice in range(0, rang -2 + 1 , 1 ):
+        Un = U0 + U1
+        U0 = U1
+        U1 = Un
+    print("fibo de ", indice + 2 ," = ", Un)
+
+#fibo d'un rang demande a l'utilisateur
 rang = input("Veuillez saisir le rang de la suite fibonacci:")
 #conversion
 rang = int(rang)
-U0 = 1
-U1 = 1
-
-for indice in range(0, rang -2 + 1 , 1 ):
-    Un = U0 + U1
-    U0 = U1
-    U1 = Un
-    print("fibo de ", indice + 2 ," = ", Un)
+fibonacci(rang)
+#fibo de 6
+print("fibonacci de 6")
+fibonacci(6)
+fibonacci()
 
